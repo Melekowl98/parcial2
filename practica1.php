acb163825fcb07796cc5d09545d103c6bd56cee5
Melekowl98 <94095971+Melekowl98@users.noreply.github.com>
2021-11-10 23:06:26 -0700

    Add files via upload

---
+++ practica1.php
@@ -0,0 +1,15 @@
+<?php
+        echo "mi primer programa en php";
+        echo "<br><br>";
+        echo "<h1> Lista de numeros</h1>";
+         
+        for($i = 0; $i<10; $i++){
+                echo $i+1 . "<br>";
+          }
+        for($i=0; $i<10; $i++){
+           echo "<div style='border: 1px solid black; padding:30px; width:30px;'>";
+           echo $i+1;
+           echo "</div>";
+      }
+          echo "Ivan Arturo Escobar Nieto";
+?>
